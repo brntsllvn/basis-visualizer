5d4dbaa2d8cdf44a519e1ab0dca1cec8a2c737a6
Brent Sullivan <brntsllvn@gmail.com>
2023-09-07 20:36:21 +0000

    add vscode to source

--- ia/main.py
+++ ia/main.py
@@ -6,41 +6,44 @@ https://adviserinfo.sec.gov/compilation
 """
 Question to answer... how much AUM managed in the USA? 
 - federal and state
-""" 
-import xml.etree.ElementTree as ET
+"""
 
+
+
+
+import xml.etree.ElementTree as ET
 def parse_firm(firm, is_state):
     firm_data = {}
-    
+
     # Parse Info
     firm_data['Info'] = firm.find('Info').attrib
-    
+
     # Parse Main Address
     firm_data['MainAddr'] = firm.find('MainAddr').attrib
-    
+
     # Parse Mailing Address
     mailing = firm.find('MailingAddr')
     firm_data['MailingAddr'] = mailing.attrib if mailing is not None else {}
-    
+
     # For convenience
     firm_data['FederalRgstn'] = not is_state
-    
+
     # Parse Registration
     if is_state:
         firm_data['StateRgstn'] = [{'Rgltr': rgltr.attrib} for rgltr in firm.findall('./StateRgstn/Rgltrs/Rgltr')]
     else:
         firm_data['Rgstn'] = firm.find('Rgstn').attrib
-    
+
     # Parse Notice Filed States
     if not is_state:
         firm_data['NoticeFiled'] = [{'States': state.attrib} for state in firm.findall('./NoticeFiled/States')]
-    
+
     # Parse Filing
     firm_data['Filing'] = firm.find('Filing').attrib
-    
+
     # Parse FormInfo
     form_info = {}
-    
+
     part1A = firm.find('./FormInfo/Part1A')
     part1A_info = {}
     for item in part1A:
@@ -56,10 +59,11 @@ def parse_firm(firm, is_state):
         for item in part1B:
             part1B_info[item.tag] = item.attrib
         form_info['Part1B'] = part1B_info
-    
+
     firm_data['FormInfo'] = form_info
-    
+
     return firm_data
+
 
 def flatten_dict(d, parent_key='', sep='.'):
     items = {}
@@ -87,8 +91,10 @@ def flatten_dict(d, parent_key='', sep='.'):
             items[new_key] = v
     return {k.replace('.States', '').replace('.Rgltrs', ''): v for k, v in items.items()}
 
+
 def flatten_list_of_dicts(list_of_dicts):
     return [flatten_dict(d) for d in list_of_dicts]
+
 
 def print_aum(flattened_firms):
     federal_total_aum = 0
@@ -105,14 +111,15 @@ def print_aum(flattened_firms):
     print(f"Federal Total AUM: {formatted_federal_total_aum}")
     print(f"State Total AUM: {formatted_state_total_aum}")
 
+
 def main():
     federal_tree = ET.parse('./ia/data/IA_FIRM_SEC_Feed_09_06_2023.xml')
     state_tree = ET.parse('./ia/data/IA_FIRM_STATE_Feed_09_06_2023.xml')
-    
+
     all_firms = []
     for tree in [federal_tree, state_tree]:
         root = tree.getroot()
-        
+
         is_state = ""
         if root.tag == 'IAPDFirmSECReport':
             is_state = False
@@ -120,14 +127,14 @@ def main():
             is_state = True
         else:
             raise "unknown tree"
-        
+
         for firm in root.findall('./Firms/Firm'):
             parsed_firm = parse_firm(firm, is_state)
             all_firms.append(parsed_firm)
-
     flattened_firms = flatten_list_of_dicts(all_firms)
-    
+    print("hello")
     print_aum(flattened_firms)
-    
+
+
 if __name__ == '__main__':
     main()
